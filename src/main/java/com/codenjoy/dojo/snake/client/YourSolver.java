6c85229cb9898bfa8357e7f254ef2f1961edf103
alihmzyv <alihmzyv@gmail.com>
2022-11-27 20:44:08 +0400

    solver first v.

--- src/main/java/com/codenjoy/dojo/snake/client/YourSolver.java
+++ src/main/java/com/codenjoy/dojo/snake/client/YourSolver.java
@@ -32,15 +32,14 @@ import com.codenjoy.dojo.services.RandomDice;
 import com.codenjoy.dojo.snake.model.Elements;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.AllDirectedPaths;
 import org.jgrapht.alg.shortestpath.JohnsonShortestPaths;
 import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.codenjoy.dojo.snake.client.PointHelper.getNeighbours;
 
@@ -51,7 +50,7 @@ public class YourSolver implements Solver<Board> {
 
 
     public static void main(String[] args) {
-        logger.info(String.format("RUN/TRY DATE/TIME: %s", LocalDateTime.now()));
+//        logger.info(String.format("RUN/TRY DATE/TIME: %s", LocalDateTime.now()));
         try {
             WebSocketRunner.runClient(
                     // paste here board page url from browser after registration
@@ -71,7 +70,6 @@ public class YourSolver implements Solver<Board> {
     private Point apple;
     private Point stone;
     private Point head;
-    private Point neck;
     private List<Point> snake;
     private List<Point> emptyPoints;
     private List<Point> barriers;
@@ -86,18 +84,18 @@ public class YourSolver implements Solver<Board> {
 
     @Override
     public String get(Board board) {
-        logger.info("Inside get(Board board)");
+//        logger.info("Inside get(Board board)");
         this.board = board;
-        long start = Instant.now().toEpochMilli();
+//        long start = Instant.now().toEpochMilli();
         try {
             fetchData();
             return solve();
         }
         catch (Exception exc) {
-            logger.error(exc.getMessage());
-            logger.info("Exception thrown in get(). RIGHT Dir. returned.");
-            logger.info(String.format("Time it took: %8.5f seconds", (Instant.now().toEpochMilli() - start) / 1000.0));
-            logger.info("Leaving get(Board board)");
+//            logger.error(exc.getMessage());
+//            logger.info("Exception thrown in get(). RIGHT Dir. returned.");
+//            logger.info(String.format("Time it took: %8.5f seconds", (Instant.now().toEpochMilli() - start) / 1000.0));
+//            logger.info("Leaving get(Board board)");
             return Direction.RIGHT.toString();
         }
     }
@@ -108,7 +106,7 @@ public class YourSolver implements Solver<Board> {
     }
 
     private void parseBoard() {
-        logger.info(String.format("The board:\n%s\n", board));
+//        logger.info(String.format("The board:\n%s\n", board));
         this.boardSize = board.size();
         this.dh = 1;
         this.dw = 1;
@@ -132,20 +130,10 @@ public class YourSolver implements Solver<Board> {
         System.out.println(graph.toString());
     }
 
-    private void addNeck() {
-        this.neck = this.snake
-                .stream()
-                .filter(point -> PointHelper.isNeigbourOf(point, head))
-                .findAny()
-                .get();
-        graph.addVertex(neck);
-        graph.addEdge(neck, head);
-    }
-
     private void addEmptyPoints() {
         emptyPoints.forEach(point -> {
             graph.addVertex(point);
-            getNeighbours(point, dw, dh, boardSize, barriers)
+            getNeighbours(point, dw, dh, boardSize, barriers, false)
                     .forEach(neighbourPoint -> {
                         graph.addVertex(neighbourPoint);
                         graph.addEdge(point, neighbourPoint);
@@ -155,7 +143,7 @@ public class YourSolver implements Solver<Board> {
 
     private void addApple() {
         graph.addVertex(apple);
-        getNeighbours(apple, dw, dh, boardSize, barriers)
+        getNeighbours(apple, dw, dh, boardSize, barriers, false)
                 .forEach(neighbourPoint ->
                 {
                     graph.addVertex(neighbourPoint);
@@ -165,7 +153,7 @@ public class YourSolver implements Solver<Board> {
 
     private void addHead() {
         graph.addVertex(head);
-        getNeighbours(head, dw, dh, boardSize, barriers)
+        getNeighbours(head, dw, dh, boardSize, barriers, false)
                 .forEach(neighbourPoint -> {
                     graph.addVertex(neighbourPoint);
                     graph.addEdge(head, neighbourPoint);
@@ -179,7 +167,7 @@ public class YourSolver implements Solver<Board> {
 
     private void addStone() {
         graph.addVertex(stone);
-        getNeighbours(stone, dw, dh, boardSize, barriers)
+        getNeighbours(stone, dw, dh, boardSize, barriers, false)
                 .forEach(neighbourPoint ->
                 {
                     graph.addVertex(neighbourPoint);
@@ -188,35 +176,35 @@ public class YourSolver implements Solver<Board> {
     }
 
     private String solve() {
-        logger.info("Inside solve()");
+//        logger.info("Inside solve()");
         long startMilli = Instant.now().toEpochMilli();
-        logger.info(String.format("Solve started: %d epoc seconds", startMilli));
+//        logger.info(String.format("Solve started: %d epoc seconds", startMilli));
         Optional<Point> nextPoint = getNextPoint();
         if (nextPoint.isEmpty()) {
             String dir = Direction.random().toString();
-            logger.fatal(String.format("Final Random direction returned: %s", dir));
+//            logger.fatal(String.format("Final Random direction returned: %s", dir));
             return dir;
         }
         String dir = PointHelper.getDir(head, nextPoint.get()).toString();
-        logger.info(String.format("Final Direction found returned: %s", dir));
-        logger.info(String.format("Solve Finished. Time it took: %8.5f seconds", (Instant.now().toEpochMilli() - startMilli) / 1000.0));
-        logger.info("Leaving solve()...");
+//        logger.info(String.format("Final Direction found returned: %s", dir));
+//        logger.info(String.format("Solve Finished. Time it took: %8.5f seconds", (Instant.now().toEpochMilli() - startMilli) / 1000.0));
+//        logger.info("Leaving solve()...");
         return dir;
     }
 
     private Optional<Point> getNextPoint() {
         Optional<Point> pointChosen;
         if (snake.size() > 70) {
-            logger.info(String.format("Snake size was higher than 70: %d", snake.size()));
+//            logger.info(String.format("Snake size was higher than 70: %d", snake.size()));
             pointChosen = getDirToPoint(apple);
         }
         else {
             pointChosen = getDirToPoint(apple);
         }
         if (pointChosen.isEmpty()) {
-            pointChosen = getDirToFurthestEmptyPointNoStone(3);
+            pointChosen = getDirToFurthestEmptyPointNoStone(null);
             if (pointChosen.isEmpty()) {
-                pointChosen = getDirToFurthestEmptyPointWithStone(3);
+                pointChosen = getDirToFurthestEmptyPointWithStone(null);
                 if (pointChosen.isEmpty()) {
                     return Optional.empty();
                 }
@@ -226,18 +214,18 @@ public class YourSolver implements Solver<Board> {
     }
 
     public Optional<Point> getDirToPoint(Point point) {
-        logger.info("Inside getDirToPoint():");
-        if (point.equals(apple)) {
-            logger.info("Going for apple..");
-        }
-        else {
-            logger.info("Going for stone");
-        }
+//        logger.info("Inside getDirToPoint():");
+//        if (point.equals(apple)) {
+//            logger.info("Going for apple..");
+//        }
+//        else {
+//            logger.info("Going for stone");
+//        }
         List<Point> obstacles = new ArrayList<>(barriers);
         obstacles.remove(head);
-        if (getNeighbours(point, dw, dh, boardSize, obstacles).size() == 1 &&
-                !getNeighbours(point, dw, dh, boardSize, obstacles).contains(head)) {
-            logger.info("The apple or stone is on the dead point. Not going..");
+        if (getNeighbours(point, dw, dh, boardSize, obstacles, false).size() == 1 &&
+                !getNeighbours(point, dw, dh, boardSize, obstacles, false).contains(head)) {
+//            logger.info("The apple or stone is on the dead point. Not going..");
             return Optional.empty();
         }
         return Optional.ofNullable(shortestPaths.getPath(head, point))
@@ -245,70 +233,67 @@ public class YourSolver implements Solver<Board> {
     }
 
     public Optional<Point> getDirToFurthestEmptyPointNoStone(Integer pathLength) {
-        logger.info("Inside getDirToFurthestEmptyPointNoStone():");
-        addNeck();
+//        logger.info("Inside getDirToFurthestEmptyPointNoStone():");
         return allPaths.getAllPaths(
-                        Set.of(neck), new HashSet<>(emptyPoints), true, pathLength)
+                        Set.of(head), new HashSet<>(emptyPoints), true, pathLength)
                 .stream()
-                .filter(path -> path.getLength() == pathLength)
-                .min(Comparator.comparing(path -> {
-                    AtomicInteger curvy = new AtomicInteger();
-                    path.getEdgeList()
-                            .forEach(edge -> {
-                                Point source = graph.getEdgeSource(edge);
-                                Point target = graph.getEdgeTarget(edge);
-                                if (source.getY() == target.getY()) {
-                                    curvy.getAndIncrement();
-                                }
-                                else {
-                                    curvy.getAndDecrement();
-                                }
-                            });
-                    return curvy.get();
-                }))
+                .max(Comparator.comparingInt(GraphPath::getLength))
+//                .min(Comparator.comparing(path -> {
+//                    AtomicInteger curvy = new AtomicInteger();
+//                    path.getEdgeList()
+//                            .forEach(edge -> {
+//                                Point source = graph.getEdgeSource(edge);
+//                                Point target = graph.getEdgeTarget(edge);
+//                                if (source.getY() == target.getY()) {
+//                                    curvy.getAndIncrement();
+//                                }
+//                                else {
+//                                    curvy.getAndDecrement();
+//                                }
+//                            });
+//                    return curvy.get();
+//                }))
                 .map(path -> {
-                    List<Point> vertices = path.getVertexList();
-                    Point nextPoint = path.getVertexList().get(2);
-                    logger.info(String.format("Path found (vertices, starting from snake piece one before head): %s", vertices));
-                    logger.info(String.format("Point returned: %s", nextPoint));
-                    logger.info("Leaving getDirToFurthestEmptyPointNoStone()...");
-                    return nextPoint;
+//                    List<Point> vertices = path.getVertexList();
+                    //                    logger.info(String.format("Path found: %s", vertices));
+//                    logger.info(String.format("Point returned: %s", nextPoint));
+//                    logger.info("Leaving getDirToFurthestEmptyPointNoStone()...");
+                    return path.getVertexList().get(1);
                 });
     }
 
     public Optional<Point> getDirToFurthestEmptyPointWithStone(Integer pathLength) {
-        logger.info("Inside getDirToFurthestEmptyPointWithStone():");
+//        logger.info("Inside getDirToFurthestEmptyPointWithStone():");
         HashSet<Point> availablePoints = new HashSet<>(emptyPoints);
-        getNeighbours(stone, dw, dh, boardSize, barriers)
+        getNeighbours(stone, dw, dh, boardSize, barriers, false)
                 .forEach(neighbourPoint -> graph.addEdge(neighbourPoint, stone));
         availablePoints.add(stone);
-        addNeck();
         return allPaths.getAllPaths(
-                        Set.of(neck), new HashSet<>(availablePoints), true, pathLength)
+                        Set.of(head), new HashSet<>(availablePoints), true, pathLength)
                 .stream()
-                .filter(path -> path.getLength() == pathLength)
-                .min(Comparator.comparingInt(path -> {
-                    AtomicInteger curvy = new AtomicInteger();
-                    path.getEdgeList()
-                            .forEach(edge -> {
-                                Point source = graph.getEdgeSource(edge);
-                                Point target = graph.getEdgeTarget(edge);
-                                if (source.getY() == target.getY()) {
-                                    curvy.getAndIncrement();
-                                }
-                                else {
-                                    curvy.getAndDecrement();
-                                }
-                            });
-                    return curvy.get();
-                }))
+                .max(Comparator.comparingInt(GraphPath::getLength))
+//                .filter(path -> path.getLength() == pathLength)
+//                .min(Comparator.comparingInt(path -> {
+//                    AtomicInteger curvy = new AtomicInteger();
+//                    path.getEdgeList()
+//                            .forEach(edge -> {
+//                                Point source = graph.getEdgeSource(edge);
+//                                Point target = graph.getEdgeTarget(edge);
+//                                if (source.getY() == target.getY()) {
+//                                    curvy.getAndIncrement();
+//                                }
+//                                else {
+//                                    curvy.getAndDecrement();
+//                                }
+//                            });
+//                    return curvy.get();
+//                }))
                 .map(path -> {
-                    List<Point> vertices = path.getVertexList();
-                    Point nextPoint = path.getVertexList().get(2);
-                    logger.info(String.format("Path found (vertices, starting from snake piece one before head): %s", vertices));
-                    logger.info(String.format("Point returned: %s", nextPoint));
-                    logger.info("Leaving getDirToFurthestEmptyPointWithStone()...");
-                    return nextPoint;
+//                    List<Point> vertices = path.getVertexList();
+                    //                    logger.info(String.format("Path found: %s", vertices));
+//                    logger.info(String.format("Point returned: %s", nextPoint));
+//                    logger.info("Leaving getDirToFurthestEmptyPointWithStone()...");
+                    return path.getVertexList().get(1);
                 });
     }
 }
